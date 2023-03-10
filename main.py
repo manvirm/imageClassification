f9b0bb6e920771baac864538402093e9486c2360
Manvir Mann <manvirm29@gmail.com>
2023-03-09 21:58:11 -0800

    Initalize main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras import datasets, layers, models
+
+(training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
