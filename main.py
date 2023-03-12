5b87312a5558acc86f8a77e0f1ebdb3d4d37572c
Manvir Mann <manvirm29@gmail.com>
2023-03-11 23:34:55 -0800

    List objects and display image

--- main.py
+++ main.py
@@ -3,4 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras import datasets, layers, models
 
+# Prepare data
 (training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
+training_images, testing_images = training_images / 255, testing_images / 255
+
+# List of objects to classify
+class_names = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
+
+for i in range(16):
+    plt.subplot(4,4, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(training_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[training_labels[i][0]])
+
+plt.show()
