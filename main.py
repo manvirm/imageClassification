c907b62f851338f4a3a3c2a8c5aa4809bbc67e61
Manvir Mann <manvirm29@gmail.com>
2023-03-12 22:27:04 -0700

    Print loss & accuracy of model and save

--- main.py
+++ main.py
@@ -43,3 +43,9 @@ model.compile(optimizer = 'adam', loss='sparse_categorical_crossentropy',
                 metrics=['accuracy'])
 
 model.fit(training_images, training_labels, epochs=10, validation_data=(testing_images, testing_labels))
+
+loss, accuracy = model.evaluate(testing_images, testing_labels)
+print(f"Loss: {loss}")
+print(f"Accuracy: {accuracy}")
+model.save('image_classifier.model')
+#model = models.load_data()
